493f334fa5d8413f07af30db7d3258c913cd6900
rarebeatle <amoralesm@ucenfotec.ac.cr>
2020-11-14 12:57:28 -0600

    Commit

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,15 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+router.get('/form', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/formulario.html'));
+});
+
+router.get('/login', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/iniciarSesion.html'));
+});
+
+
 module.exports = router;
 
 //   res.sendFile(path.join(__dirname+'/index.html'));
