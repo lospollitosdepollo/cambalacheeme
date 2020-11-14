a3c935d19d26344ff0a06298fa743a1847b09dff
rarebeatle <amoralesm@ucenfotec.ac.cr>
2020-11-14 13:12:29 -0600

    perfil usuario

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,23 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+<<<<<<< Updated upstream
+=======
+router.get('/form', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/formulario.html'));
+});
+
+router.get('/login', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/iniciarSesion.html'));
+});
+
+router.get('/userprofile', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/perfil.html'));
+});
+
+
+
+>>>>>>> Stashed changes
 module.exports = router;
 
 //   res.sendFile(path.join(__dirname+'/index.html'));
